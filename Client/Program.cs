18ac740dbdc263146513f44bbc92d965c3a1bdeb
Cootz <artem2004799@gmail.com>
2023-11-12 16:27:00 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Net.Sockets;
+
+Memory<byte> buffer = new byte[] { 0,1,2,3,4,5,6,7 };
+int openedConnections = 0;
+
+while (true)
+{
+    try
+    {
+        TcpClient client = new();
+
+        await client.ConnectAsync(args[0], 16000);
+
+        await client.Client.SendAsync(buffer, SocketFlags.None);
+
+        client.Close();
+
+        openedConnections++;
+    }
+    catch (Exception ex)
+    { 
+        Console.WriteLine(ex);
+    }
+
+    if (openedConnections % 1000 == 0)
+    {
+        Console.WriteLine(openedConnections);
+    }
+}
---
+++ TcpSocketsLimitTesting/Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+
+internal class Program
+{
+    public static Dictionary<IPEndPoint, List<DateTime>> Connections { get; } = new();
+
+    private static async Task Main(string[] args)
+    {
+        System.Timers.Timer timer = new();
+        timer.Elapsed += (_,_) => displayReceivedConnections();
+        timer.Interval = 2000; // in miliseconds
+        
+        TcpListener listener = new(IPAddress.Parse(args[0]), 16000);
+
+        timer.Start();
+        listener.Start();
+
+        try
+        {
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+
+                IPEndPoint clientEndPoint = (IPEndPoint)client.Client.RemoteEndPoint!;
+
+                bool connectionExists = Connections.TryGetValue(clientEndPoint, out var connectionDateTimes);
+
+                if (connectionExists)
+                {
+                    connectionDateTimes!.Add(DateTime.Now);
+                }
+                else
+                {
+                    Connections.Add(clientEndPoint, new List<DateTime>() { DateTime.Now });
+                }
+
+                _ = ProcessTcpClient(client);
+            }
+        }
+        finally
+        {
+            displayReceivedConnections();
+        }
+
+        static void displayReceivedConnections()
+        {
+            DateTime lastTwoMin = DateTime.Now - TimeSpan.FromMinutes(2);
+
+            int connectionsReceived = 0;
+
+            foreach (KeyValuePair<IPEndPoint, List<DateTime>> connection in Connections)
+            {
+                foreach (DateTime dateTime in connection.Value)
+                {
+                    if (dateTime >= lastTwoMin)
+                    {
+                        connectionsReceived++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Connections received: {connectionsReceived}");
+        }
+    }
+
+    public static async Task ProcessTcpClient(TcpClient client)
+    {
+        Memory<byte> buffer = new byte[512];
+
+        await client.Client.ReceiveAsync(buffer, SocketFlags.None);
+
+        client.Close();
+    }
+}
